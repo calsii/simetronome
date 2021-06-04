042edc77e0236fadae0ed8ff9f76709760fdd3e9
nick <nick@codium.com.au>
2021-06-04 15:48:29 +0930

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Speed msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import Speed from './components/Speed.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family:
+    -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans",
+    "Helvetica Neue", Arial, sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Speed.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a href="#" @click.prevent="bpmDown">&#10134</a>
+    {{ bpm.count }}
+    <a href="#" @click.prevent="bpmUp">&#10133</a>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+const bpm = reactive({ count: 120 })
+const bpmUp = () => bpm.count++
+const bpmDown = () => bpm.count <= 0 ? bpm.count = 0 : bpm.count--
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
