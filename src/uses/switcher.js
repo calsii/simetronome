cb9571f7ced6b34c4d1e21209e6fcf7c738f1a27
nick <nick@codium.com.au>
2021-06-11 11:49:42 +0930

    add audio

--- src/uses/switcher.js
+++ src/uses/switcher.js
@@ -1,13 +1,21 @@
 import { reactive, computed } from 'vue'
+import sound from '@/assets/mixkit-positive-interface-beep-221.wav'
+
+const audio = new window.Audio(sound)
 
 const beepingEffect = bpm => {
   bpm.flash = !bpm.flash
-  // TODO
-  // add sound effect
+
+  if (bpm.flash) {
+    audio.play()
+  } else {
+    audio.pause()
+  }
 }
 
 const beeping = ({ switcher, bpm }) => {
   bpm.flash = false
+  audio.pause()
 
   if (switcher.on) {
     switcher.timer = window.setInterval(() => beepingEffect(bpm), 60.0 / bpm.value * 1000.0)
